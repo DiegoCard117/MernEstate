bd9b11ef5b2245a87a8e54661f1c1380848cf953
DiegoCard117 <diegocardoso117@gmail.com>
2023-10-19 19:41:05 -0300

    create and add OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,22 +1,30 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://www.forestcom.com.br/wp-content/uploads/2018/09/blank-profile-picture-973460_640.png",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-},{ timestamps: true });
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-export default User
+export default User;
