de8fd089305a683ba2e4e20fbd544646203a578e
DiegoCard117 <diegocardoso117@gmail.com>
2023-10-24 13:37:12 -0300

    user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,38 @@
-export const test = (request, response) => {
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+
+export const test = (_request, response) => {
   response.json({
-    message:"Is Working!!"
-  })
-}
+    message: "Is Working!!",
+  });
+};
+
+export const updateUser = async (request, response, next) => {
+  if (request.user.id !== request.params.id)
+    return next(errorHandler(401, "You can only update your own account!"));
+  try {
+    if (request.body.password) {
+      request.body.password = bcryptjs.hashSync(request.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      request.params.id,
+      {
+        $set: {
+          username: request.body.username,
+          password: request.body.password,
+          email: request.body.email,
+          avatar: request.body.avatar,
+        },
+      },
+      { new: true }
+    );
+
+    const {password, ...rest} = updatedUser._doc
+
+    response.status(200).json(rest)
+  } catch (error) {
+    next(error);
+  }
+};
